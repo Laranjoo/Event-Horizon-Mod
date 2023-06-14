0efb41f8015ee4d43fd0f148265f708c05bd2eeb
boomdoom123 <122758329+boomdoom123@users.noreply.github.com>
2023-06-14 15:50:58 -0400

    Update ShockedPlayer.cs

--- Buffs/ShockedPlayer.cs
+++ Buffs/ShockedPlayer.cs
@@ -2,22 +2,27 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace EventHorizons.Players
+namespace EventHorizons.Buffs
 {
     public class ShockedPlayer : ModPlayer
     {
-        public bool HasGalvaniteArmor = true;
+        public bool HasGalvaniteArmor = false;
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
             int chance = Main.rand.Next(3);
-            if (chance == 0)
+            if (HasGalvaniteArmor = true)
             {
-                if (Player.HeldItem.DamageType == DamageClass.Magic)
-                {
-                    target.AddBuff(ModContent.BuffType<Buffs.Shocked>(), 180);
-                }
-                else if (Player.HeldItem.DamageType == DamageClass.Ranged)
-                { target.AddBuff(ModContent.BuffType<Buffs.Shocked>(), 180); }
+               if (chance == 0)
+                    {
+                        if (Player.HeldItem.DamageType == DamageClass.Magic)
+                            {
+                                target.AddBuff(ModContent.BuffType<Buffs.Shocked>(), 180);
+                            }
+                        else if (Player.HeldItem.DamageType == DamageClass.Ranged)
+                            { 
+                                 target.AddBuff(ModContent.BuffType<Buffs.Shocked>(), 180); 
+                            }
+                    }
             }
         }
     }
