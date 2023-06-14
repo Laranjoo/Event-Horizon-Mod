8b8043c676988773bc801278b0939c9449b7a25e
boomdoom123 <122758329+boomdoom123@users.noreply.github.com>
2023-06-14 16:59:04 -0400

    Update GalvaniteLeggings.cs

--- Armor/GalvaniteLeggings.cs
+++ Armor/GalvaniteLeggings.cs
@@ -2,7 +2,6 @@ using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 using Terraria.GameContent.Creative;
-using EventHorizons.Items.Armor;
 using EventHorizons.Items.Placeables;
 
 namespace EventHorizons.Armor
